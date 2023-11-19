5b96fc626c0bcbc46d828672c06c3a2699995dfb
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-19 22:04:01 +0330

    implement create ad api

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+from user.models import User
 
 
 class Ad(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=1023)
-    # author = models.ForeignKey("User", null=True, on_delete=models.RESTRICT)
+    author = models.ForeignKey(User, null=True, on_delete=models.RESTRICT)
--- advertisement/views.py
+++ advertisement/views.py
@@ -7,5 +7,16 @@ from advertisement.serializers import AdSerializer
 
 class AdvertiementView(APIView):
     def get(self, request, ad_id=None):
-        ad = Ad.objects.filter(id=ad_id)
+        ad = Ad.objects.filter(id=ad_id).get()
         return Response(AdSerializer(ad).data)
+
+    def put(self, request):
+        title = request.data.get('title')
+        content = request.data.get('content')
+        if not title or not content:
+            return Response(status=400)
+        ad = Ad()
+        ad.title = title
+        ad.content = content
+        ad.save()
+        return Response(data={'id': ad.id}, status=201)
