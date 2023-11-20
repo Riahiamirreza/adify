6cc5548825e56d15c82504383a16350a536e079c
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-20 20:18:09 +0330

    sync AppConfig by environment variables

--- advertisement/apps.py
+++ advertisement/apps.py
@@ -1,6 +1,15 @@
+import os
+
 from django.apps import AppConfig
 
+from dotenv import load_dotenv
 
 class AdvertisementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'advertisement'
+
+    @classmethod
+    def _init_(cls, env_path: str = '.env'):
+        load_dotenv(env_path)
+
+AdvertisementConfig._init_()
