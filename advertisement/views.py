7be4a6cf5309e949c88f0eaafc6b4656b795d5b3
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-20 22:45:38 +0330

    implement list ads api

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -7,11 +10,20 @@ from advertisement.serializers import AdSerializer
 
 class AdvertisementView(APIView):
     def get(self, request, ad_id=None):
-        if not (qs := Ad.objects.filter(id=ad_id).all()).exists():
-            return Response(status=404)
-        ad = qs.get()
-        return Response(AdSerializer(ad).data)
+        try:
+            if ad_id is None:
+                limit = int(request.query_params.get('limit', '10'))
+                offset = int(request.query_params.get('offset', '0'))
+                qs = Ad.objects.all()[offset:offset+limit]
+                return Response(AdSerializer(qs, many=True).data)
+            if not (qs := Ad.objects.filter(id=ad_id).all()).exists():
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            ad = qs.get()
+            return Response(AdSerializer(ad).data)
+        except Exception as exc:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    # @login_required(login_url='/login')
     def post(self, request):
         title = request.data.get('title')
         content = request.data.get('content')
