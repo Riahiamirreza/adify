b408b193dc83ec94edbcbdd13a55333b670e47e7
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-20 21:30:35 +0330

    implement LoginView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import authenticate, login
+
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -6,3 +9,13 @@ from user.serializers import UserSerializer
 
 class UserView(APIView):
     ...
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if (user := authenticate(request, username=username, password=password)) is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        login(request=request, user=user)
+        return Response(status=status.HTTP_200_OK)
