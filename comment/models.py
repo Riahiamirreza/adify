f45fd6250003de1764c3c0ab32fc2311960102a4
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-20 21:00:47 +0330

    add created-at and updated-at fields to advertisement model

--- comment/models.py
+++ comment/models.py
@@ -6,3 +6,4 @@ from advertisement.models import Ad
 class Comment(models.Model):
     content = models.TextField(null=False, max_length=1023)
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add==True)
