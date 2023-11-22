aa3aa541f38a0e7f33246c4101a075440774ad3c
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-22 20:36:51 +0330

    fix tests, add user authentication for delete-ad api

--- advertisement/tests.py
+++ advertisement/tests.py
@@ -1,10 +1,13 @@
 from unittest.mock import patch
 
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APIRequestFactory
 
 from advertisement.models import Ad
 from advertisement.views import AdvertisementView
+
+from user.models import User
 
 
 class TestGetAdvertisement(TestCase):
@@ -30,14 +33,19 @@ class TestGetAdvertisement(TestCase):
         response = AdvertisementView().get(request=request, ad_id=123)  # does not exist
         self.assertEqual(response.status_code, 404)
 
+
 class TestDeleteAdvertisement(TestCase):
     @classmethod
     def setUp(cls):
-        Ad(id=1, title='title-1', content='content-1').save()
+        cls.test_user_one = User.objects.create_user(username='un-1', password='123', email='a@a.a')
+        cls.test_user_two = User.objects.create_user(username='un-2', password='456', email='b@b.b')
+        cls.factory = APIRequestFactory()
+        Ad(id=1, title='title-1', content='content-1', author=cls.test_user_one).save()
+        Ad(id=2, title='title-2', content='content-2', author=cls.test_user_two).save()
 
     def test_delete_advertisement(self):
-        factory = APIRequestFactory()
-        request = factory.get('/advertisement/x')
+        request = self.factory.get('/advertisement/x')
+        request.user = self.test_user_one
 
         self.assertTrue(Ad.objects.filter(id=1).exists())
         response = AdvertisementView().delete(request=request, ad_id=1)
@@ -45,8 +53,19 @@ class TestDeleteAdvertisement(TestCase):
         self.assertFalse(Ad.objects.filter(id=1).exists())
 
     def test_delete_nonexistent_advertisement(self):
-        factory = APIRequestFactory()
-        request = factory.get('/advertisement/x')
+        request = self.factory.get('/advertisement/x')
+        request.user = self.test_user_one
 
         response = AdvertisementView().delete(request=request, ad_id=123)
         self.assertEqual(response.status_code, 404)
+
+    def test_delete_advertisement_by_unauthorized_user(self):
+        request = self.factory.get('/advertisement/x')
+        request.user = self.test_user_two
+
+        response = AdvertisementView().delete(request=request, ad_id=1)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        request.user = self.test_user_one
+        response = AdvertisementView().delete(request=request, ad_id=2)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
