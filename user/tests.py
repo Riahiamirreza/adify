13e58c2ed2df97ffaf5b14c86e06bfe6becd1ef7
Riahiamirreza <riahiamirreza79@gmail.com>
2023-11-22 23:46:27 +0330

    cover more edge cases in test-create-duplicate-user

--- user/tests.py
+++ user/tests.py
@@ -29,6 +29,18 @@ class TestCreateUser(TestCase):
         response = UserView().post(request=request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+        user_data = {'username': 'un-1', 'password': 'StrongPass', 'email': 'new@mail.ok'}
+        request = self.request_factory.post('/user', data=user_data)
+        request.data = user_data
+        response = UserView().post(request=request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_data = {'username': 'new', 'password': 'StrongPass', 'email': 'un1@mail.ok'}
+        request = self.request_factory.post('/user', data=user_data)
+        request.data = user_data
+        response = UserView().post(request=request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_create_user_by_invalid_parameters(self):
         user_data = {'username': 'Reza', 'password': 'StrongPass'}
         request = self.request_factory.post('/user', data=user_data)
